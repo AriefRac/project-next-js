c461f8574e8be38bc175f1de4f709406df8ee5fc
Arief <muhariefrac@gmail.com>
2023-10-29 19:56:09 +0700

    test use client side

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,17 @@
+"use client"
+
 import AnimeList from '@/components/AnimeList'
 import Header from '@/components/AnimeList/Header'
 import { getAnimeResponse, getNestedAnimeResponse } from '@/libs/api-libs'
 
-import seedrandom from 'seedrandom'
 
 const Page = async () => {
 
   const topAnime = await getAnimeResponse("top/anime", "limit=8")
   let recAnime = await getNestedAnimeResponse("recommendations/anime", "entry")
-  const rng = seedrandom()
-  let Random = rng() * 193
-  console.log(rng());
+  let Random = Math.floor(Math.random() * 193)
   console.log(Random);
+
 
   recAnime = { data: recAnime.slice(Random, Random + 8) }
 
